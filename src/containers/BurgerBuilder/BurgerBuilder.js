f872b1dc2b0590bfe40e0ea7f6adf494937777f9
ByungJun Cho <aischool815@gmail.com>
2018-08-08 20:50:38 +0900

    lesson 116

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import Aux from '../../hoc/Auxiliary'
+import Burger from '../../components/Burger/Burger'
 export default class BurgerBuilder extends Component {
   render() {
     return (
       <Aux>
-        <div>Burger</div>
+        <Burger />
         <div>Burger Controls</div>
       </Aux>
     )
