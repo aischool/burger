b8f4ca81a4ebb87913ed7b64750d3b1501ab5870
ByungJun Cho <aischool815@gmail.com>
2018-08-08 19:44:50 +0900

    lesson 112

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Burglar
+        <Layout>  
+          <p>Burglar</p>
+        </Layout>
       </div>
     );
   }
