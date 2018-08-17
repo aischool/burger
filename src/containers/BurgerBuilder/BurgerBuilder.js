aecdeaac73c21781368c79dd396409910e21b9cb
ByungJun Cho <aischool815@gmail.com>
2018-08-17 23:48:45 +0900

    lesson 171

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
@@ -14,7 +15,7 @@ const INGREDIENT_PRICES = {
   bacon: 0.7
 };
 
-export default class BurgerBuilder extends Component {
+class BurgerBuilder extends Component {
   // constructor(props) {
   //   super(props)
   //   this.state = {...}
@@ -102,7 +103,7 @@ export default class BurgerBuilder extends Component {
     axios
       .post('/orders.json', order)
       .then(response => this.setState({ loading: false, purchasing: false }))
-      .catch(error => this.setState({ loading: true, purchasing: false }));
+      .catch(error => this.setState({ loading: false, purchasing: false }));
   };
 
   render() {
@@ -145,3 +146,5 @@ export default class BurgerBuilder extends Component {
     );
   }
 }
+
+export default withErrorHandler(BurgerBuilder, axios);
